f3c939563c35d8ea7bc3b24ed55694b45065165d
Eunice Lim <eunicelim31@gmail.com>
2021-06-17 10:49:28 -0500

    week 4

---
+++ Week 4/di-composer-app/src/app/composer.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { IComposer } from './composer.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ComposerService {
+
+  composers: Array<IComposer>;
+
+  constructor(){
+    this.composers= [
+      { composerId: 100, fullName: "Franz Liszt", genre: "Classical"},
+      { composerId: 101, fullName: "Ludwig van Beethoven", genre: "Classical"},
+      { composerId: 102, fullName: "Johann Sebastian Bach ", genre: "Classical"},
+      { composerId: 103, fullName: "Sergei Rachmaninoff", genre: "Classical"},
+      { composerId: 104, fullName: "Pyotr Ilyich Tchaikovsky", genre: "Classical"},
+      ]
+   }
+   getComposers(){
+     return this.composers;
+   }
+   getComposer(composerId: number){
+     for (let composer of this.composers){
+       if (composer.composerId === composerId){
+         return composer;
+       }
+     }
+      return {} as IComposer;
+   }
+      }
